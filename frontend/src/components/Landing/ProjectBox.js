ae141b7926c12a06142df0036f734d5488f64688
trudypainter <tpainter@mit.edu>
2022-01-01 19:21:21 -0500

    fix thumb path

--- frontend/src/components/Landing/ProjectBox.js
+++ frontend/src/components/Landing/ProjectBox.js
@@ -3,39 +3,61 @@ import ProjectItem from "./Project/ProjectItem";
 import React, { useEffect, useState } from "react";
 
 const ProjectBox = () => {
-  // const [projects, updateProjects] = useState([]);
-  // useEffect(() => {
-  //   fetch("/api/landing")
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       updateProjects(response.data);
-  //     });
-  // }, []);
+  const [projects, updateProjects] = useState([]);
+  useEffect(() => {
+    fetch("/api/landing")
+      .then((response) => response.json())
+      .then((response) => {
+        updateProjects(response.data);
+      });
+  }, []);
 
-  const projects = [
-    {
-      title: "test1",
-      location: "blank",
-      year: "year",
-      description: "description",
-      image:
-        "https://raw.githubusercontent.com/trudypainter/trudy-computer-react/main/projects/real-talk-radio/rtroverview.jpg",
-      url: "blank",
-      description: "test description",
-      tags: ["tag 1", "tag two"],
-    },
-    {
-      title: "test2",
-      location: "blank",
-      year: "year",
-      description: "description",
-      url: "blank",
-      image:
-        "https://github.com/trudypainter/trudy-computer-react/blob/main/projects/spotify-monster/FINAL_WRITEUP/images/overview.jpeg?raw=true",
-      description: "test description",
-      tags: ["tag 1", "tag two"],
-    },
-  ];
+  // const projects = [
+  //   {
+  //     title: "test1",
+  //     location: "blank",
+  //     year: "year",
+  //     description: "description",
+  //     image:
+  //       "https://github.com/trudypainter/trudy-computer-react/blob/main/projects/thumbnails/spotify.png?raw=true",
+  //     url: "blank",
+  //     description: "test description",
+  //     tags: ["tag 1", "tag two"],
+  //   },
+  //   {
+  //     title: "test2",
+  //     location: "blank",
+  //     year: "year",
+  //     description: "description",
+  //     url: "blank",
+  //     image:
+  //       "https://raw.githubusercontent.com/trudypainter/trudy-computer-react/main/projects/thumbnails/rtr.png?raw=true",
+  //     description: "test description",
+  //     tags: ["tag 1", "tag two"],
+  //   },
+  //   {
+  //     title: "test2",
+  //     location: "blank",
+  //     year: "year",
+  //     description: "description",
+  //     url: "blank",
+  //     image:
+  //       "https://raw.githubusercontent.com/trudypainter/trudy-computer-react/main/projects/thumbnails/rtr.png?raw=true",
+  //     description: "test description",
+  //     tags: ["tag 1", "tag two"],
+  //   },
+  //   {
+  //     title: "test2",
+  //     location: "blank",
+  //     year: "year",
+  //     description: "description",
+  //     url: "blank",
+  //     image:
+  //       "https://raw.githubusercontent.com/trudypainter/trudy-computer-react/main/projects/thumbnails/rtr.png?raw=true",
+  //     description: "test description",
+  //     tags: ["tag 1", "tag two"],
+  //   },
+  // ];
 
   return (
     <div className="projectBox">
