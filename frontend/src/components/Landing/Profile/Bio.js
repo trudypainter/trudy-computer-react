e09391e60489fe7fe528ceb9078a8bb50097450c
trudypainter <tpainter@mit.edu>
2022-06-09 09:16:22 -0400

    added license plate link

--- frontend/src/components/Landing/Profile/Bio.js
+++ frontend/src/components/Landing/Profile/Bio.js
@@ -9,13 +9,22 @@ const Bio = () => {
       </p>
       <p>
         Researcher at the MIT Media Lab with the{" "}
-        <a href="https://www.media.mit.edu/groups/viral-communications/overview/">
+        <a
+          target="_blank"
+          href="https://www.media.mit.edu/groups/viral-communications/overview/"
+        >
           Viral Communications Group.
         </a>
       </p>
       <p>
         I like building web systems from scratch, phone photography, archives,
-        biking, and custom license plates.
+        biking, and{" "}
+        <a
+          target="_blank"
+          href="https://www.icloud.com/sharedalbum/#B0X5ejO17JIX8pl"
+        >
+          custom license plates.
+        </a>
       </p>
       <p>
         📬 <a href="mailto:tpainter@mit.edu">tpainter@mit.edu</a>
